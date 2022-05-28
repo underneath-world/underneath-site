f24f9910f953d208795cba673275661c61e74670
Alexa Bonomo <me@alexaann.net>
2022-05-27 20:54:02 -0700

    add print

--- handmade-dataset/public/sketch.js
+++ handmade-dataset/public/sketch.js
@@ -3,7 +3,8 @@ var socket;
 function setup(){
     createCanvas(200,200)
     background(0);
-    socket = io.connect('https://damp-stream-62424.herokuapp.com/');
+    socket = io.connect('http://localhost:3000');
+    // socket = io.connect('https://damp-stream-62424.herokuapp.com/');
     socket.on('mouse', newDrawing);
 }
 
@@ -30,3 +31,32 @@ function mouseDragged(){
 function draw(){
 
 }
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    const thisMoment = Object.fromEntries(data.entries());
+
+    console.log({
+        thisMoment
+    });
+
+    const results = document.querySelector('.results pre');
+    results.innerText = JSON.stringify(thisMoment, null, 2);
+    document.querySelector('.results').style.display = "block";
+    document.querySelector('.this-moment').style.display = "none";
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
+function display() {
+        window.print();
+
+        socket.on('private', function(msg) {
+            alert(msg);
+        });
+}
+
--- handmade-dataset/server.js
+++ handmade-dataset/server.js
@@ -1,17 +1,38 @@
 var express = require ('express');
 
 var app = express();
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(3000);
+// var server = app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'));
 
 var socket = require('socket.io');
 var io = socket(server);
+console.log("socket server is running")
+
+const users = [];
+var user= -1;
 
 io.sockets.on('connection', newConnection)
 
+
+
 function newConnection(socket){
+    user++;
+
+    users.push({
+        userID: socket.id,
+        user: user
+    });
     console.log('new connection: ' + socket.id);
+
+      socket.emit("users", users);
+      console.log(users);
+      
+      // ...
+
+    
+    io.to(socket.id).emit('private', 'hi');
 
     socket.on('mouse', mouseMsg)
 
@@ -21,9 +42,17 @@ function newConnection(socket){
         //include all sockets including yourself
         //io.sockets.emit('mouse',data);
         console.log(data);
+
     }
     
-    socket.on('disconnect', () => console.log('Client disconnected'));
+    
+    socket.on('disconnect', disconnect)
+    
+    function disconnect(){
+        console.log('Client disconnected');
+    }
+    
+    
 }
 
-console.log("socket server is running")
+
